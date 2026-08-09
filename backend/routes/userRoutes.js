078d4b1186a2538a348fbb80fd470c74fefb0de9
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-09 17:31:36 +0600

    fix login issue

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 let express = require("express");
-const {registerUser,loginUser} = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 let router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authMiddleware, (req, res) => {
+  res.status(200).json({
+    status: 1,
+    message: "Authentication successful",
+    user: req.user,
+  });
+});
 
 module.exports = router;
